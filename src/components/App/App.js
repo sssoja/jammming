b7aa4f3f51c56f281aee37583add87954e807190
Sophie <sophie@sssoja.com>
2021-10-12 19:55:17 +0100

    render tracklist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./App.css";
 
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [
+        {
+          name: "some track name",
+          artist: "some artist",
+          album: "some album",
+          id: 1,
+        },
+        {
+          name: "some track name",
+          artist: "some artist",
+          album: "some album",
+          id: 2,
+        },
+        {
+          name: "some track name",
+          artist: "some artist",
+          album: "some album",
+          id: 3,
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
@@ -9,7 +40,11 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <div className="App-playlist"></div>
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
     );
